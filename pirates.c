1336a33583c9b5deea2a6127fd78acec22e04742
David Mihal <eastsideski@gmail.com>
2012-09-19 21:25:19 -0400

    started pirate

---
+++ pirates.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define PIRATE 1
+#define NINJA  2
+
+void pirate();
+pthread_mutex_t store_status = PTHREAD_MUTEX_INITIALIZER;
+
+main()
+{
+	pthread_t pirates[12];
+	pthread_t ninjas[4];
+    pthread_create()
+
+}
+void pirate(int type){
+
+}
