bf855e1188472971399304294e522c7455bbb78c
David Mihal <eastsideski@gmail.com>
2012-09-25 18:46:32 -0400

    more updates

--- traffic.c
+++ traffic.c
@@ -19,11 +19,13 @@
 #define EAST 1
 #define SOUTH 2
 #define WEST 3
+const char *directions[] = {"North","South","East","West"};
 
 // Turn directions
 #define LEFT 0
 #define STRAIGHT 1
 #define RIGHT 2
+const char *turns[] = {"Left","Straight","Right"};
 
 // Quadrants
 #define NW 0
@@ -32,8 +34,6 @@
 #define SW 3
 
 void handler (void *ptr);
-char* approachToString(int n);
-char* turnToString(int n);
 void lockSpaces(int turns[4]);
 void print_intersection();
 char int2char(int i);
@@ -49,7 +49,7 @@ int main() {
 	unsigned int seed = (unsigned int)time(NULL);
 	srand(seed);
     pthread_t car[NUM_CARS];
-    sem_init(&mutex, 0, 1);
+    //sem_init(&mutex, 0, 1);
     sem_init(&spaces[0],0,1);
     sem_init(&spaces[1],0,1);
     sem_init(&spaces[2],0,1);
@@ -58,74 +58,51 @@ int main() {
     for (j = 1; j <= NUM_CARS; j++) {
     	approach_direction[j] = rand() % 4;
     	turn_direction[j] = rand() % 3;
-    	pthread_create(&car[j], NULL, (void *)&handler, &j);
-    	pthread_join(car[j], NULL);
+    	pthread_create(&car[j], NULL, (void *)&handler, (void *) j);
   	} 
-    sem_destroy(&mutex);     
+    //sem_destroy(&mutex);     
+    sleep(20);
     return 0;
 }
 
 void handler(void *ptr) {
+
     int x; 
-    x = *((int *) ptr);
+    x = (int) ptr;
+
     int a = approach_direction[x];
     int t = turn_direction[x];
     int turn[4] = {0,0,0,0};
     
-    printf("Car %d: Approaching intersection from the %s, wants to turn %s", x, approachToString(a), turnToString(t));
-    sem_wait(&mutex);
-    	// Critical Region
-		printf("        \n", x);
-		printf("       Waits for possibility\n");
+    printf("Car %d: Approaching intersection from the %s, wants to turn %s \n", x, directions[a], turns[t]);
 		
-		if (turn[x] == LEFT) {
-			turn[(a + 2) % 4] = 1;
-			turn[(a + 3) % 4] = 1;
-    		turn[a] = 1;
-    		lockSpaces(turn);
-    		passthrough(a, x);
-    		passthrough((a + 3) % 4, x);
-    		passthrough((a + 2) % 4, x);
-    	} else if (turn[x] == STRAIGHT) {
-    		turn[(a + 3) % 4] = 1;
-    		turn[a] = 1;
-    		lockSpaces(turn);
-    		passthrough((a + 3) % 4, x);
-    		passthrough(a, x);
-    	} else if (turn[x] == RIGHT) {
-    		turn[a] = 1;
-    		lockSpaces(turn);
-    		passthrough(a, x);
-    	}
-		
-		printf("       Turns %s\n", turnToString(turn[x]));
-    sem_post(&mutex);
+	if (turn[x] == LEFT) {
+		turn[(a + 2) % 4] = 1;
+		turn[(a + 3) % 4] = 1;
+		turn[a] = 1;
+		lockSpaces(turn);
+		passthrough(a, x);
+		passthrough((a + 3) % 4, x);
+		passthrough((a + 2) % 4, x);
+	} else if (turn[x] == STRAIGHT) {
+		turn[(a + 3) % 4] = 1;
+		turn[a] = 1;
+		lockSpaces(turn);
+		passthrough((a + 3) % 4, x);
+		passthrough(a, x);
+	} else if (turn[x] == RIGHT) {
+		turn[a] = 1;
+		lockSpaces(turn);
+		passthrough(a, x);
+	}
+	
     pthread_exit(0);
-}
-
-char* approachToString(int n) {
-	if (n == NORTH)
-		return "North";
-	if (n == EAST)
-		return "East";
-	if (n == SOUTH)
-		return "South";
-	if (n == WEST)
-		return "West";
-}
-
-char* turnToString(int n) {
-	if (n == LEFT)
-		return "Left";
-	if (n == STRAIGHT)
-		return "Straight";
-	if (n == RIGHT)
-		return "Right";
 }
 
 void passthrough(int quad, int car_num) {
 	quadrant[quad] = car_num;
 	print_intersection();
+	sleep(2);
 	quadrant[quad] = 0;
 	sem_post(&spaces[quad]);
 }
@@ -145,19 +122,17 @@ void lockSpaces(int turns[4]){
 	}
 }
 void print_intersection(){
-	printf("      │   │   │\n"
-"      │   │   │\n"
+	printf("\n      │   N   │\n"
 "      │   │   │\n"
 "      │   │   │\n"
 "──────┘───│───└──────\n"
 "      │ %c │ %c │\n"
-"──────────┼──────────\n"
+"W─────────┼─────────E\n"
 "      │ %c │ %c │\n"
 "──────┐───│───┌──────\n"
 "      │   │   │\n"
 "      │   │   │\n"
-"      │   │   │\n"
-"      │   │   │\n",int2char(quadrant[0]),int2char(quadrant[1]),int2char(quadrant[3]),int2char(quadrant[2]));
+"      │   S   │\n",int2char(quadrant[0]),int2char(quadrant[1]),int2char(quadrant[3]),int2char(quadrant[2]));
 }
 char int2char(int i){
 	return (i==0) ? ' ' : ('0'+i);
