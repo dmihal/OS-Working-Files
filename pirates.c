6ca41e49e8f3b2c848ed05a2603ebb13374b9299
David Mihal <eastsideski@gmail.com>
2012-09-25 10:37:46 -0400

    Working without checking other seat

--- pirates.c
+++ pirates.c
@@ -9,78 +9,79 @@
 #define NUM_NINJAS  4
 
 void* person(void *typept);
-void leaveStore();
+int enterStore(int type);
+void leaveStore(int seat);
+void printSeats();
 
 pthread_mutex_t store_status_mutex = PTHREAD_MUTEX_INITIALIZER;
 int store_status = 0;
 
 pthread_mutex_t store_customers_mutex = PTHREAD_MUTEX_INITIALIZER;
 int store_customers = 0;
+int seats[2];
 
-const char *types[] = {"Pirate","Ninja"};
+const char *types[] = {"Empty ","Pirate","Ninja "};
 
 main()
 {
 	pthread_t pirates[NUM_PIRATES];
 	pthread_t ninjas[NUM_NINJAS];
 	int i;
+	seats[0]=0;
+	seats[1]=0;
+
+	for (i = 0; i < NUM_NINJAS; ++i)
+	{
+		printf("Making ninja %d\n", i);
+		pthread_create(&pirates[i],NULL,&person,NINJA);
+	}
 	for (i = 0; i < NUM_PIRATES; ++i)
 	{
 		printf("Making pirate %d\n", i);
 		pthread_create(&pirates[i],NULL,&person,PIRATE);
 	}
-    for (i = 0; i < NUM_NINJAS; ++i)
-	{
-		printf("Making ninja %d\n", i);
-		pthread_create(&pirates[i],NULL,&person,NINJA);
-	}
+    
 	sleep(30);
 }
 void *person(void *typept){
 	printf("Yo\n");
 	int type = (int) typept;
+	int seat;
 	while(1){
-		printf("Wait for Customers\n");
 		pthread_mutex_lock(&store_customers_mutex);
-		if(store_customers==0){
-			store_customers++;
+		if (!seats[0] || !seats[1])
+		{
+			seat = enterStore(type);
+			printSeats();
 			pthread_mutex_unlock(&store_customers_mutex);
-			printf("Wait for Status\n");
-			pthread_mutex_lock(&store_status_mutex);
-			store_status = type;
-			pthread_mutex_unlock(&store_status_mutex);
-			printf("%s has entered the store\n", types[type-1]);
-			sleep(3);
-			leaveStore();
-			printf("%s has left the store\n", types[type-1]);
-		} else if(store_customers==1) {
-			pthread_mutex_unlock(&store_customers_mutex);
-			printf("Wait for Status 2\n");
-			pthread_mutex_lock(&store_status_mutex);
-			if(store_status==type){
-				pthread_mutex_unlock(&store_status_mutex);
-				enterStore();	
-				
+			if (-1 != seat)
+			{
 				sleep(3);
-				leaveStore();
-				printf("%s has left the store\n", types[type-1]);
-			} else {
-				pthread_mutex_unlock(&store_status_mutex);
-				pthread_mutex_unlock(&store_customers_mutex);
+				leaveStore(seat);
 			}
+		} else {
+			pthread_mutex_unlock(&store_customers_mutex);
 		}
-		printf("Loop\n");
 	}
 }
-void enterStore(){
-	printf("Wait for Customers, trying to emter\n");
-	pthread_mutex_lock(&store_customers_mutex);
-	store_customers++;
-	pthread_mutex_unlock(&store_customers_mutex);
+int enterStore(int type){
+	if (!seats[0])
+	{
+		seats[0] = type;
+		return 0;
+	} else if (!seats[1])
+	{
+		seats[1] = type;
+		return 1;
+	} else {
+		return -1;
+	}
 }
-void leaveStore(){
-	printf("Wait for Customers, trying to leave\n");
-	pthread_mutex_lock(&store_customers_mutex);
-	store_customers--;
-	pthread_mutex_unlock(&store_customers_mutex);
+void leaveStore(int seat){
+	seats[seat]=0;
+}
+void printSeats(){
+	printf("┌──────┬──────┐\n" );
+	printf("│%s│%s│\n", types[seats[0]],types[seats[1]]);
+	printf("└──────┴──────┘\n" );
 }
