bab5ea252e600959010c9de8c984210b31ac2f5b
Dylan Kirby <djkirby@wpi.edu>
2012-09-30 23:40:38 -0400

    added some buffer functionality

--- malloc.c
+++ malloc.c
@@ -7,16 +7,33 @@
 #define ALLOC_SIZE 32
 
 void *meh_malloc1(unsigned int size);
-void *my_sbrk(unsigned int size);
 
 typedef struct {
 	void *ptr;
-	int size, used;
+	int size;
 } Cell;
 
 void *meh_malloc1(unsigned int size) {  
-	void *ptr;
-	return (ptr = my_sbrk(ALLOC_SIZE)) ? ptr : NULL;
+	void *start, *end, *ptr;
+
+	// larger than buffer size
+	if (size > ALLOC_SIZE)
+		return sbrk(size);
+
+	// buffer needs to be expanded
+	if (end - start < size) {
+		if ((ptr = sbrk(ALLOC_SIZE)) == NULL)
+			return NULL;
+		if (ptr != end)
+			start = ptr;
+		end = ptr + ALLOC_SIZE;
+	}
+
+	// use part of buffer
+	ptr = start;
+	start += ALLOC_SIZE;
+
+	return (ptr ? ptr : NULL);
 }
 
 int main() {
@@ -26,38 +43,11 @@ int main() {
 	arr[0].size = ALLOC_SIZE;
 	arr[0].ptr = meh_malloc1(ALLOC_SIZE);
 	printf("Allocating %d bits\n", ALLOC_SIZE);
-	printf("After allocating: %x\n", sbrk(0));
+	printf("After allocating: %x\n\n", sbrk(0));
 
 	printf("Before alloc: %x\n", sbrk(0));
 	arr[1].size = ALLOC_SIZE;
 	arr[1].ptr = meh_malloc1(ALLOC_SIZE);
 	printf("Allocating %d bits\n", ALLOC_SIZE);
-	printf("After allocating: %x\n", sbrk(0));
-}
-
-void *my_sbrk(unsigned int size) {
-	static char *start = NULL, *end = NULL;
-	char *ptr;
-
-	// larger than buffer size
-	if (size > ALLOC_SIZE)
-		return sbrk(size);
-
-	// buffer needs to be expanded
-	if (end - start < size) {
-		ptr = sbrk(ALLOC_SIZE);
-		if (ptr == NULL)
-			return NULL;
-			
-		// if buffers aren't adjacent, reset start
-		if (ptr != end)
-			start = ptr;
-		end = ptr + ALLOC_SIZE;
-	}
-
-	// use part of buffer
-	ptr = start;
-	start += (size + 7) & ~7;
-
-	return ptr;
+	printf("After allocating: %x\n\n", sbrk(0));
 }
