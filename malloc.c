d3d0e46dadae863f6d09adf149c349d1818ad56f
Dylan Kirby <djkirby@wpi.edu>
2012-09-30 23:31:20 -0400

    added my_sbrk function

--- malloc.c
+++ malloc.c
@@ -7,6 +7,7 @@
 #define ALLOC_SIZE 32
 
 void *meh_malloc1(unsigned int size);
+void *my_sbrk(unsigned int size);
 
 typedef struct {
 	void *ptr;
@@ -15,7 +16,7 @@ typedef struct {
 
 void *meh_malloc1(unsigned int size) {  
 	void *ptr;
-	return (ptr = sbrk(size)) ? ptr : NULL;
+	return (ptr = my_sbrk(ALLOC_SIZE)) ? ptr : NULL;
 }
 
 int main() {
@@ -32,4 +33,31 @@ int main() {
 	arr[1].ptr = meh_malloc1(ALLOC_SIZE);
 	printf("Allocating %d bits\n", ALLOC_SIZE);
 	printf("After allocating: %x\n", sbrk(0));
+}
+
+void *my_sbrk(unsigned int size) {
+	static char *start = NULL, *end = NULL;
+	char *ptr;
+
+	// larger than buffer size
+	if (size > ALLOC_SIZE)
+		return sbrk(size);
+
+	// buffer needs to be expanded
+	if (end - start < size) {
+		ptr = sbrk(ALLOC_SIZE);
+		if (ptr == NULL)
+			return NULL;
+			
+		// if buffers aren't adjacent, reset start
+		if (ptr != end)
+			start = ptr;
+		end = ptr + ALLOC_SIZE;
+	}
+
+	// use part of buffer
+	ptr = start;
+	start += (size + 7) & ~7;
+
+	return ptr;
 }
