b2f897330229a1a45cfcda029fca840b0b2713b4
David Mihal <eastsideski@gmail.com>
2012-10-04 19:01:16 -0400

    idk'

--- malloc.c
+++ malloc.c
@@ -1,10 +1,11 @@
+// Dylan Kirby djkirby
+// David Mihal dimihal
+// Project 4
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
-#define ALLOC_SIZE 32
 
 void *meh_malloc1(unsigned int size);
 
@@ -13,9 +14,30 @@ typedef struct {
 	int size, used;
 } Cell;
 
-void *meh_malloc1(unsigned int size) {  
+void* malloc_addr;
+int free_size;
+
+void *meh_malloc1(unsigned int size) {
+	int* sizeblock;
 	void *ptr;
-	return (ptr = sbrk(size)) ? ptr : NULL;
+	if (malloc_addr && free_size >= (size+8))
+	{
+		ptr = malloc_addr;
+		malloc_addr = malloc_addr + size + 8;
+		free_size = free_size - size - 8;
+	} else {
+		ptr = sbrk(size*2+16);
+		if(!ptr){
+			return NULL;
+		}
+
+		malloc_addr = ptr + size + 8;
+		free_size = size-8;
+	}
+	sizeblock = ptr;
+	(*sizeblock) = size;
+	
+	return ptr +8;
 }
 
 int main() {
