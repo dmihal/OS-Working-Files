f8f0ee7a74b4960c0de306f5acdc0f85716d3b92
Dylan Kirby <djkirby@wpi.edu>
2012-10-05 16:22:42 -0400

    fixed struct

--- malloc.c
+++ malloc.c
@@ -11,14 +11,8 @@ typedef struct {
 	unsigned int size;
 	void* prev;
 	void* next;
-} chunk;
-
-typedef struct {
-	void* ptr;
-	unsigned int size;
 } header;
 
-const int chunksize = sizeof(chunk);
 const int headsize = sizeof(header);
 
 void* malloc_addr;
@@ -71,7 +65,7 @@ void *calloc(size_t count, size_t size) {
 
 void printMemory()
 {
-	chunk* c = malloc_head;
+	header* c = malloc_head;
 	while(c)
 	{
 		printf("╔════════════╗\n"
